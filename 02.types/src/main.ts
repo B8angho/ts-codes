acca7e79752f36d2a8dce63561b28830b59c53e0
B8angho <bhjeon98@gmail.com>
2023-12-04 11:40:45 +0900

    update main.ts

--- 02.types/src/main.ts
+++ 02.types/src/main.ts
@@ -49,3 +49,59 @@ let tuple2: [string, number];
 tuple2 = ["a", 1];
 tuple2.push(2);
 console.log(tuple2);
+
+// any (모든 타입 할당)
+let any: any = "abc";
+any = 1;
+any = [];
+
+// unknown
+let unknown: unknown = false;
+// let string1: boolean = unknown;
+let string2: boolean = unknown as boolean;
+
+// object
+let obj: object = {};
+let arr: object = {};
+// let nul: object = null;
+let date: object = new Date();
+
+const obj1: { id: number; title: string; desc: string } = {
+  id: 1,
+  title: "title1",
+  desc: "description1",
+};
+
+// union
+let union: string | number;
+union = "hi";
+union = 123;
+// union = [];
+
+// Function
+let func1: (arg1: number, arg2: number) => number;
+func1 = (x, y) => {
+  return x * y;
+};
+
+let func2: () => void;
+func2 = () => {
+  console.log("hi");
+};
+
+// Null, Undefined
+// void
+
+// never
+function throwError(): never {
+  throw new Error("error");
+}
+
+function keepProcessing() {
+  while (true) {
+    console.log("hi");
+  }
+}
+
+const never: number[] = [];
+never.push(1);
